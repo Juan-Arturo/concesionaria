4a6997cbec7a3b0edb37c46cedb84a8aaa5db8f0
abrahammendez11 <abraham.mndz11@hotmail.com>
2023-07-04 15:05:57 -0600

    Galeria

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { presupuestoComponent } from './Presupuesto/presupuesto.component';
+import { galeriaComponent } from './Galeria/galeria.component';
+import { inicioComponent } from './Inicio/inicio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    presupuestoComponent
+    presupuestoComponent,
+    galeriaComponent,
+    inicioComponent
 
   ],
   imports: [
